897e6dede1cd5e1daa80dbbb1217cb536bcc2ae7
OysteinKoder <oystein.kodehode@gmail.com>
2021-08-25 14:02:25 +0200

    patch 0.202
    Minor changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ChildBookImg from "../img/ChildBooksOnline.jpg"
 
 function Projects() {
     return (
@@ -6,11 +7,16 @@ function Projects() {
             <h2>Projects</h2>
             <div className="White-box">
                 <div className="Grid-project">
-                    <img className="Project-Img"></img>
-                    <div className="Project-h2"></div>
-                    <div className="Project-p"></div>
+                    <div className="Project-Img"></div>
+                    <div className="Project-h2"> <h2>Child Books Online</h2></div>
+                    <div className="Project-p"> 
+                    <p>
+                        Child books Online is a fun interactive app that
+                        let kids learn new words in an interactive way, 
+                        it also teaches them basic smart-device skills.
+                        </p>
+                    </div>
                     <div className="Project-link"></div>
-
                 </div>
             </div>
         </div>
