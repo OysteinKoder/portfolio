ceabc5168e984936f7eda5ac4c1158ba15e19b60
OysteinKoder <oystein.kodehode@gmail.com>
2021-08-26 14:43:49 +0200

    patch 0.205

--- src/App.js
+++ src/App.js
@@ -13,9 +13,18 @@ import Projects from "./components/projects"
 function App() {
   return (
     <Router>
-<Header></Header>
+      <Header></Header>
       <Skills></Skills>
-      <Projects></Projects>
+      <Projects
+        description="Child Books Online is a fun and interactive app that i am developing for mobile and pads.
+                        When ready it will be released on google play and apple store. 
+                        The goal is to provide parents with an interactive and fun way to teach their kids new words and basic smart-device use. And all of this for a much cheaper price then buying picture books. This is a work in progress."
+
+        title="Child Books Online"
+        imgClass="Child-books-online-img"
+        link="https://oysteinkoder.github.io/child_books_online/Animals">
+
+      </Projects>
     </Router>
   );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,21 +1,18 @@
 import React from "react"
 import ChildBookImg from "../img/ChildBooksOnline.jpg"
 
-function Projects() {
+function Projects(props) {
     return (
         <div className="Component-spacing Project-scroll">
             <h2>Projects</h2>
             <div className="White-box">
                 <div className="Grid-project">
-                    <div className="Project-img"></div>
-                    <span className="Project-h2-2"> Child Books Online</span>
+                    <div className={props.imgClass}></div>
+                    <span className="Project-h2-2"> {props.title} </span>
                     <div className="Project-p"> 
-                    
-                        Child Books Online is a fun and interactive app that
-                        let kids learn new words in an interactive way, 
-                        it also teaches them basic smart-device skills.
+                        {props.description}
                     </div>
-                    <div className="Project-link"></div>
+                    <button className="Project-link"> <a href={props.link}>Click</a></button>
                 </div>
             </div>
         </div>
