4d38d6ea1506f96952b0aead641c7b2cb6e17854
Oystein Koder <75257876+OysteinEllingsen@users.noreply.github.com>
2021-09-03 14:14:27 +0200

    patch 0.305
    ..

--- src/components/skills.js
+++ src/components/skills.js
@@ -5,6 +5,8 @@ import CssIcon from "../img/cssIcon.png"
 import JsIcon from "../img/jsIcon.png"
 import FigmaIcon from "../img/figmaIcon.png"
 
+const Novice = document.getElementById("Novice")
+
 function Skills() {
     return ( 
     <div className="Skill-component">
@@ -12,7 +14,7 @@ function Skills() {
         </div>
             <h2 className="H2-headline">Skills</h2>
                 <div className="Grid Skills-animation">
-                    <h3 className="Novice">Novice <QuestionSvg class="Svg"></QuestionSvg></h3>
+                    <h3 className="Novice" onClick={test}>Novice <QuestionSvg class="Svg"></QuestionSvg></h3>
                     <h3 className="Junior">Junior <QuestionSvg class="Svg"></QuestionSvg></h3>
                     <h3 className="Professional">Pro <QuestionSvg class="Svg"></QuestionSvg></h3>
                     <h3 className="Expert">Expert <QuestionSvg class="Svg"></QuestionSvg></h3>
@@ -24,11 +26,20 @@ function Skills() {
                     <div className="Css-bar"></div>
                     <div className="Js-bar"></div>
                     <div className="Figma-bar"></div>
-                
         </div>
-            
+        <div className="Explain Novice-explain" onClick={noviceShow}> Has some knowlage but always needs guidence</div>
+        <div className="Explain Junior-explain"> Can work independantly but needs the occational guidence</div>
+        <div className="Explain Pro-explain"> Has loads of experiance and rearly needs any help</div>
+        <div className="Explain Expert-explain"> Can work independantly but needs the occational guidence</div>
             </div>
     )
+}
+
+function noviceShow() {
+    let noviceClicked 
+    if (Novice.click) {
+        console.log("turtle")
+    }
 }
 
 export default Skills
